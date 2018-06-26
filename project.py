35e4ab213ca7771e22febe8b538a2f0ddfb9ba9a
ashley-young <36734565+ashley-young@users.noreply.github.com>
2018-06-26 14:33:39 -0400

    Update project.py
    changed the structure of the list to have one dict per index and the confidence level is the maximum one

--- project.py
+++ project.py
@@ -1,6 +1,4 @@
 
-# coding: utf-8
-
 # # Object Detection Demo
 # Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md) before you start.
 
