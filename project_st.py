b1282b301bb43796b3b939b1e212f1cf38a92b18
ashley-young <36734565+ashley-young@users.noreply.github.com>
2018-07-06 13:37:15 -0400

    Update and rename project_st (2).py to project_st.py

--- project_st.py
+++ project_st.py
@@ -9,7 +9,7 @@
 # In[8]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 import numpy as np
 import os
 import six.moves.urllib as urllib
